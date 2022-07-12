78fa5203851cabb29ef049bd3d240c073937ed1d
Jennifer <niceswteacher@gmail.com>
2022-07-12 23:50:28 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://aus1:sydney1@cluster0.7lcyt.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(config.mongoURI
 ).then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => {res.send('안녕하세요 !!!')})
+app.get('/', (req, res) => {res.send('<b> 안녕 ? <br> 반가워 ')})
 
-app.listen(port, () => {
-  console.log('Example app listening on port ${port}')
+app.post('/register', (req, res) => {
+  // 회원가입에 필요한 정보를 client에서 가져와서 
+  // 서버에 저장
+  const user = new User(req.body)
+  
+  user.save( (err, doc) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
+
 })
+
+app.listen(port, () => console.log('Example app listening on port ${port}'))
